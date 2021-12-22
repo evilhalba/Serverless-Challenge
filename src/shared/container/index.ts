fa73c78d226ee7a755d2971e507da6014e0cbef7
Eduardo Vilhalba <vilhalba.20000@gmail.com>
2021-12-22 09:39:39 -0300

    dependency injection configurantion and default app error config

---
+++ src/shared/container/index.ts
@@ -0,0 +1,8 @@
+import { container } from "tsyringe";
+import { EmployeeRepository } from "../../modules/employee/infra/typeorm/repositories/EmployeeRepository";
+import IEmployeeRepository from "../../modules/employee/repositories/IEmployeeRepository";
+
+container.registerSingleton<IEmployeeRepository>(
+  'EmployeeRepository',
+  EmployeeRepository
+);
